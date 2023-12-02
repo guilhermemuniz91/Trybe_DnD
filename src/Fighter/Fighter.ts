df680f73a5778ca21232c462bb8eb50d7f028977
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-12-02 17:15:30 -0300

    req07 feito

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,21 +1,3 @@
-// import Energy from '../Energy';
-
-// export interface GenericFighter<T> {
-//   lifePoints: number;
-//   strength: number;
-//   defense: number;
-//   energy?: Energy;
-
-//   attack(enemy: T): void;
-//   special?(enemy: T): void;
-// }
-
-// export default interface Fighter extends GenericFighter<Fighter>{
-
-//   receiveDamage(attackPoints: number): number;
-//   levelUp(): void;
-// }
-
 import Energy from '../Energy';
 
 interface Fighter {
